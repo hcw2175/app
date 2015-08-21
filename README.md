3e881afab4afe4d9b59272a124b651866b7ef59e
hcw <hcw2175@163.com>
2015-08-21 17:03:14 +0800

    完善APP框架：1、添加自动构建合并打包并压缩appjs以及第三方js库任务，以减少js请求访问或js依赖效果；2、添加常用js库依赖，如ngCordova,restangularjs.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ionic
 ionic demos
+
+### 开始
+
+请分别执行以下命令以初始化项目：
+
+1. npm install
+2. bower install
+3. gulp install
+4. ionic state restore
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,64 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 var bower = require('bower');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-var sh = require('shelljs');
+var ngAnnotate = require('gulp-ng-annotate');
+var uglify = require('gulp-uglify');
+var mainBowerFiles = require('main-bower-files');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+    sass: ['./scss/**/*.scss'],
+    js: ['./www/js/app.js', './www/js/**/*.js']
 };
 
-gulp.task('watch', function() {
+// 打包scss成css并压缩
+gulp.task('sass', function (done) {
+    gulp.src(paths.sass)
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        .pipe(gulp.dest('./www/css/'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({extname: '.min.css'}))
+        .pipe(gulp.dest('./www/css/'))
+        .on('end', done);
+});
+
+// 打包app的js并压缩
+gulp.task("concatJS", function (done) {
+    gulp.src(paths.js)
+        .pipe(ngAnnotate())
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./www/lib/'))
+        .pipe(uglify())
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(gulp.dest('./www/lib/'))
+        .on('end', done);
+});
+
+// 打包其它依赖js库
+// 根据bower.json中dependencies配置打包
+gulp.task("bowerJS", function(){
+    var bowerJss = mainBowerFiles();
+    console.log(bowerJss);
+
+    gulp.src(bowerJss)
+        .pipe(concat('vender.js'))
+        .pipe(gulp.dest('./www/lib/'))
+        .pipe(uglify())
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(gulp.dest('./www/lib/'))
+});
+
+// 监听任务
+gulp.task('watch', function () {
     gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.js, ['concatJS']);
 });
 
-// ���*.scss��*.css
-gulp.task('sass', function(done) {
-  gulp.src(paths.sass)
-    .pipe(sass({
-      errLogToConsole: true
-    }))
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
-});
-
-
+// 打包安装任务
+gulp.task('install', ['sass', 'concatJS', 'bowerJS']);
